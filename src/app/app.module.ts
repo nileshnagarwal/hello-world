9dbc3bd0a9bef5685a3f8f45fffdf9dfbc250bdc
nileshnagarwal <eternalshenron@gmail.com>
2017-10-22 23:26:48 +0530

    Lecture 91: Building a Bootstrap Form (Section 8 - Signup Form)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import { LikeComponent } from './like/like.component';
 import { InputFormatDirective } from './input-format.directive';
 import { ZippyComponent } from './zippy/zippy.component';
 import { ContactFormComponent } from './contact-form/contact-form.component';
+import { SignupFormComponent } from './signup-form/signup-form.component';
 
 @NgModule({
   declarations: [
@@ -30,7 +31,8 @@ import { ContactFormComponent } from './contact-form/contact-form.component';
     LikeComponent,
     InputFormatDirective,
     ZippyComponent,
-    ContactFormComponent 
+    ContactFormComponent,
+    SignupFormComponent 
   ],
   imports: [
     BrowserModule,
