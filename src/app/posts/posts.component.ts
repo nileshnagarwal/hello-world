7ab12edf70786b3dd99377bc0d227710fe9f2875
nileshnagarwal <eternalshenron@gmail.com>
2017-11-15 23:26:21 +0530

    Lecture 116, 117, 118: Throwing application specific errors, handling bad request errors and importing observable operators and factory methods

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { PostService } from '../services/post.service';
+import { AppError } from '../common/app-error';
+import { NotFoundError } from '../common/not-found-error';
+import { BadInput } from '../common/bad-input';
 
 @Component({
   selector: 'posts',
@@ -19,8 +22,8 @@ export class PostsComponent implements OnInit{
           this.posts = response.json();
         }, 
         (error : Response) => {
-          if(error.status === 400){
-            // this.form.setErrors({error.json()})
+          if(error instanceof BadInput){
+            // this.form.setErrors(error.originalError);
           }
           else
             alert("An unexpected error occured");
@@ -66,11 +69,13 @@ export class PostsComponent implements OnInit{
           let index = this.posts.indexOf(post);
           this.posts.splice(index,1);
         }, 
-        (error : Response) => {
-          if(error.status === 404)
+        (error : AppError) => {
+          if(error instanceof NotFoundError)
             alert("The post has already been deleted");  
-          else
+          else {
             alert("An unexpected error occured");
+            console.log(error);
+          }            
         }
       );
   }
--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -1,5 +1,11 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
+import 'rxjs/add/operator/catch';
+import { Observable } from 'rxjs/Observable';
+import { AppError } from '../common/app-error';
+import { NotFoundError } from '../common/not-found-error';
+import { BadInput } from '../common/bad-input';
+import 'rxjs/add/observable/throw';
 
 @Injectable()
 export class PostService {
@@ -9,7 +15,12 @@ export class PostService {
   private url = 'https://jsonplaceholder.typicode.com/posts'; 
 
   getPosts(){
-    return this.http.get(this.url);
+    return this.http.get(this.url)
+      .catch((error: Response) => {
+        if(error.status === 400)
+          return Observable.throw(new BadInput(error.json()));
+        return Observable.throw(new AppError(error.json()));
+      });
   }
 
   createPost(post){
@@ -21,6 +32,11 @@ export class PostService {
   }
 
   deletePost(id){
-    return this.http.delete(this.url + '/' + id);
+    return this.http.delete(this.url + '/' + id)
+      .catch(( error : Response ) => {
+        if(error.status === 404)
+          return Observable.throw(new NotFoundError());
+        return Observable.throw(new AppError(error));
+      });
   }
 }
