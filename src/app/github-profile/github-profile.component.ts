14f07bb968325d28a21c0fb6d8bb4882e2ca5fb1
nileshnagarwal <eternalshenron@gmail.com>
2017-11-27 03:13:29 +0530

    Lecture 132: Why Route Parameters are Observables?

--- src/app/github-profile/github-profile.component.ts
+++ src/app/github-profile/github-profile.component.ts
@@ -11,10 +11,16 @@ export class GithubProfileComponent implements OnInit {
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    // let id = this.route.snapshot.paramMap.get('id'); 
+    // console.log('id');
+    // Alternative to using paramMap as an observable is 
+    // to use the actual paramMap object. 
+    // Explained in lecture 132.
+    
     this.route.paramMap
       .subscribe(params => {
         let id = +params.get('id');
-        console.log(id);
+        console.log(params);
       });
   }
 
