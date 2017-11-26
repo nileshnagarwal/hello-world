d9fc1f120dc4e76adc0273537cb1103be17d5f7f
nileshnagarwal <eternalshenron@gmail.com>
2017-11-27 04:15:31 +0530

    Lecture 134: Query Parameters

--- src/app/github-followers/github-followers.component.ts
+++ src/app/github-followers/github-followers.component.ts
@@ -1,5 +1,6 @@
 import { GithubFollowersService } from './../services/github-followers.service';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'github-followers',
@@ -9,9 +10,15 @@ import { Component, OnInit } from '@angular/core';
 export class GithubFollowersComponent implements OnInit {
   followers: any[];
 
-  constructor(private service: GithubFollowersService) { }
+  constructor(
+    private route: ActivatedRoute,
+    private service: GithubFollowersService) { }
 
   ngOnInit() {
+
+    this.route.paramMap.subscribe();
+
+    this.route.queryParamMap.subscribe();
     this.service.getAll()
       .subscribe(followers => this.followers = followers);
   }
